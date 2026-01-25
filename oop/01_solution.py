3690d0eae52a78e431dc86505ba692a9696f03a4
Prolay <prolaydas130@Gmail.com>
2026-01-25 21:17:02 +0530

    Polymorphism

--- oop/01_solution.py
+++ oop/01_solution.py
@@ -8,6 +8,8 @@ class Car:
 
     def get_brand(self):
         return self.__brand
+    def fuel_type(self):
+        return "Petrol or Diesel"
 
 
 my_car = Car("Toyota", "Corolla")
@@ -23,9 +25,17 @@ class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
+        
+    def fuel_type(self):
+        return "Electric charge"
 
 
 my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
-print(my_tesla.battery_size)
-print(my_tesla.fullName())
-print(my_tesla.get_brand())
+# print(my_tesla.battery_size)
+# print(my_tesla.fullName())
+# print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
+
+safari = Car("Tata", "Safari")
+print(safari.fuel_type())
+

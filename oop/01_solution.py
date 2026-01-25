be89fbe14deecde02147519c62d19a4a9e6a528d
Prolay <prolaydas130@Gmail.com>
2026-01-25 21:27:12 +0530

    class variables

--- oop/01_solution.py
+++ oop/01_solution.py
@@ -1,13 +1,17 @@
 class Car:
+    total_car = 0
+
     def __init__(self, brand, model):
         self.__brand = brand
         self.model = model
+        Car.total_car += 1
 
     def fullName(self):
         return f"Brand:{self.__brand}, Model:{self.model}"
 
     def get_brand(self):
         return self.__brand
+
     def fuel_type(self):
         return "Petrol or Diesel"
 
@@ -25,7 +29,7 @@ class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
-        
+
     def fuel_type(self):
         return "Electric charge"
 
@@ -38,4 +42,4 @@ print(my_tesla.fuel_type())
 
 safari = Car("Tata", "Safari")
 print(safari.fuel_type())
-
+print(Car.total_car)
